a9a2bbfdb9d45dffeb89381d1f5dc249a4a0bfed
Rafael Roman <rafael.roman@gft.com>
2016-08-12 00:50:47 +0200

    remove unused

--- test/index.spec.js
+++
@@ -1,9 +0,0 @@
-import dataFeedOrchestrator from '../src/lib/';
-import { expect } from 'chai';
-
-
-describe('dataFeedOrchestrator', function () {
-  it('Should be true', function () {
-    return expect(dataFeedOrchestrator()).to.be.true;
-  });
-});

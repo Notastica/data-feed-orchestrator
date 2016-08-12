3cb884f21d8d767aec5c7f92d1859d54635e2768
Rafael Roman <panthro.rafael@gmail.com>
2016-08-13 01:14:35 +0200

    adding findMatchingModules and its tests

--- src/orchestrator.js
+++ src/orchestrator.js
@@ -1,8 +1,10 @@
 import Loki from 'lokijs';
 import logger from './logging/logger';
 import uuid from 'node-uuid';
+import Module from './model/module';
+import jsonpath from 'jsonpath';
 
-let db, modules;
+let db, modulesCollection;
 let initialized = false;
 let order = 0;
 
@@ -37,46 +39,60 @@ export const init = (options) => {
     logger.info('Initializing Orchestrator');
     options = options || defaultOptions;
     db = new Loki(options.dbName);
-    modules = db.addCollection(options.modules);
+    modulesCollection = db.addCollection(options.modules);
     initialized = true;
     resolve(initialized);
   });
 };
 
 /**
+ * Check if the module is of type {Module} if not convert to it
+ *
+ * @param {*|Module} module
+ * @return {Module}
+ */
+const checkModule = (module) => {
+  return typeof module === Module ? module : new Module(module);
+};
+
+/**
  * Unregister a module in the Orchestrator
- * @param {Object} module
- * @return {*|Promise}
+ * @param {Module} module
+ * @return {Promise}
  */
 export const unregister = (module) => {
 
-  return checkInit().then(() => {
-    const previousModule = modules.find({ uuid: module.uuid });
+  return checkInit()
+    .then(() => {
+      module = checkModule(module);
+      const previousModule = modulesCollection.find({ uuid: module.uuid });
 
-    if (previousModule) {
-      logger.info(`Unregistering ${module.name} with uuid ${module.uuid}`);
-      modules.remove(previousModule);
-    }
-  });
+      if (previousModule) {
+        logger.info(`Unregistering ${module.name} with uuid ${module.uuid}`);
+        modulesCollection.remove(previousModule);
+      }
+    });
 
 };
 
 /**
  * Check if a module is registered
- * @param {Object} module
+ * @param {Module} module
  * @return {boolean}
  */
 export const isRegistered = (module) => {
-  return modules.find({ uuid: module.uuid }).length > 0;
+  module = checkModule(module);
+  return modulesCollection.find({ uuid: module.uuid }).length > 0;
 };
 
 /**
  * Register a module in the Orchestrator
- * @param {Object} module
+ * @param {Module} module
  * @return {*|Promise}
  */
 export const register = (module) => {
   return checkInit().then(() => {
+    module = checkModule(module);
     logger.info('Registering new module', module);
 
     if (!module.uuid) {
@@ -86,12 +102,52 @@ export const register = (module) => {
     if (isRegistered(module)) {
       logger.info(`Module ${module.name} already registered for uuir ${module.uuid}`);
       logger.info('Unregistering previously registered module, to register the new one');
-      return unregister(modules.find({ uuid: module.uuid })[0])
+      return unregister(modulesCollection.find({ uuid: module.uuid })[0])
         .then(() => register(module));
     }
     module.order = order++;
-    return modules.insert(module);
+    return modulesCollection.insert(module);
   });
+};
+
+/**
+ * Checks if the given object matches a jsonpath
+ * @param {*} obj
+ * @param {Path} path
+ * @return {boolean}
+ */
+const matchesPath = (obj, path) => {
+  return jsonpath.query(obj, path).length > 0;
+};
+
+/**
+ * Find modules that matches for the give message
+ * ordered by their registration order
+ * @param {Object} message
+ * @return {Promise}
+ */
+export const findMatchingModules = (message) => {
+  return new Promise((resolve) => {
+    logger.debug('Finding modules that matches', message);
+    const modules = modulesCollection.where((module) => {
+      let matches = false;
+
+      if (module.positivePath) {
+        matches = matchesPath(message, module.positivePath);
+      }
+
+      if (module.negativePath) {
+        matches = !matchesPath(message, module.negativePath);
+      }
+      return matches;
+    });
+
+    logger.debug('Found modules:', modules.length);
+    resolve(modules);
+
+  });
+
+
 };
 
 
--- test/orchestrator.spec.js
+++ test/orchestrator.spec.js
@@ -6,6 +6,7 @@ import * as chai from 'chai';
 import dirtyChai from 'dirty-chai';
 import * as Orchestrator from '../src/orchestrator';
 import uuid from 'node-uuid';
+import Module from '../src/model/module';
 
 // TEST SETUP
 // =============================================================================
@@ -27,20 +28,24 @@ describe('Orchestrator', function () {
   it('Should reregister a module if already registered', function () {
     return Orchestrator.init().then(() => {
       const name = 'test-processor';
-      const keyValue = 'keyValue';
-      const updatedKeyValue = 'UpdatedKeyValue';
+      const negativePath = '$';
+      const updatedKeyValue = '$..name';
       const moduleUUID = uuid.v4();
 
-      return Orchestrator.register({ name: name, uuid: moduleUUID, key: keyValue }).then((module) => {
+      return Orchestrator.register(new Module({
+        name: name,
+        uuid: moduleUUID,
+        negativePath: negativePath
+      })).then((module) => {
         chai.expect(module).not.to.be.undefined();
         chai.expect(module.uuid).not.to.be.undefined();
         chai.expect(module.name).to.be.equals(name);
         return module;
       }).then((module) => {
-        module.key = updatedKeyValue;
+        module.negativePath = updatedKeyValue;
         return Orchestrator.register(module).then((updatedModule) => {
           chai.expect(updatedModule.uuid).to.be.equals(moduleUUID);
-          chai.expect(updatedModule.key).to.be.equals(updatedKeyValue);
+          chai.expect(updatedModule.negativePath).to.be.equals(updatedKeyValue);
         });
       });
     });
@@ -71,6 +76,70 @@ describe('Orchestrator', function () {
         return Orchestrator.unregister(module).then(() => {
           chai.expect(Orchestrator.isRegistered(module)).to.be.false();
         });
+      });
+    });
+  });
+
+  it('Should find a module by negativePath', function () {
+    return Orchestrator.init().then(() => {
+      const originalModule = new Module();
+
+      originalModule.negativePath = '$.negativeKeyName';
+
+      return Orchestrator.register(originalModule).then(() => {
+        return Orchestrator.findMatchingModules({ key: 'value' });
+      }).then((modules) => {
+        chai.expect(modules).to.be.a('array');
+        chai.expect(modules[0])
+          .to.have.property('uuid')
+          .that.is.equals(originalModule.uuid);
+      });
+    });
+  });
+
+  it('Should NOT find a module by negativePath', function () {
+    return Orchestrator.init().then(() => {
+      const originalModule = new Module();
+
+      originalModule.negativePath = '$.key';
+
+      return Orchestrator.register(originalModule).then(() => {
+        return Orchestrator.findMatchingModules({ key: 'value' });
+      }).then((modules) => {
+        chai.expect(modules).to.be.a('array');
+        chai.expect(modules).to.be.empty();
+      });
+    });
+  });
+
+  it('Should find a module by positivePath', function () {
+    return Orchestrator.init().then(() => {
+      const originalModule = new Module();
+
+      originalModule.positivePath = '$.positiveKeyName';
+
+      return Orchestrator.register(originalModule).then(() => {
+        return Orchestrator.findMatchingModules({ positiveKeyName: 'value' });
+      }).then((modules) => {
+        chai.expect(modules).to.be.a('array');
+        chai.expect(modules[0])
+          .to.have.property('uuid')
+          .that.is.equals(originalModule.uuid);
+      });
+    });
+  });
+
+  it('Should NOT find a module by positivePath', function () {
+    return Orchestrator.init().then(() => {
+      const originalModule = new Module();
+
+      originalModule.positivePath = '$.BadPositiveKeyName';
+
+      return Orchestrator.register(originalModule).then(() => {
+        return Orchestrator.findMatchingModules({ positiveKeyName: 'value' });
+      }).then((modules) => {
+        chai.expect(modules).to.be.a('array');
+        chai.expect(modules).to.be.empty();
       });
     });
   });

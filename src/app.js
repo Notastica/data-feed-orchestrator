b1e63d9df8444eff59ad57398fa4442b952fab83
Rafael Roman <panthro.rafael@gmail.com>
2016-08-16 21:21:19 +0200

    fixing npm package

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import Orchestrator from './orchestrator';
+import logger from './logging/logger';
+import uuid from 'node-uuid';
+import * as mq from './mq/connection';
+
+const o = new Orchestrator({
+  registerQueue: uuid.v4(),
+  messagesQueue: uuid.v4(),
+  messagesIndex: uuid.v4()
+});
+
+mq.connect(o.amqpURL).then((context) => {
+  const pub = context.socket('PUSH');
+  const messageSent = { uuid: uuid.v4() };
+
+  pub.connect(o.messagesQueue);
+  pub.write(JSON.stringify(messageSent));
+
+})
+  .then(o.listen())
+  .then(() => {
+    logger.info(`Orchestrator ${o.name} ready to receive new modules in the queue ${o.registerQueue}`);
+  });
+
+process.on('SIGINT', function () {
+  logger.warn('Gracefully shutting down from SIGINT (Ctrl-C)');
+  o.shutdown();
+});
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import O from './orchestrator';
+import M from './module';
+
+export const Orchestrator = O;
+export const Module = M;
